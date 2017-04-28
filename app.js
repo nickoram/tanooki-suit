9217879b144f17988b570f1fb0e61c7b9c948149
Nicholas Oram <nickoram@gmail.com>
2017-04-28 19:36:53 -0300

    editorconfig gitignore and more parsing

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 var config = require('./config/local/config');
 
 config.forEach(function(item) {
-  
+
   var record = {
     id: item.id,
     orderId: item.order_id,
+    variantId: item.variant_id,
+    variant: item.variant_name,
     name: item.first_name,
     city: item.city,
     province: item.province,
     country: item.country,
     created: item.created_at
   };
-  
+
   console.log(record);
 });
